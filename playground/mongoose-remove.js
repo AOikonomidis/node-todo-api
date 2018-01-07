1cb2bc0fcb774c63d491212e15a49d2ac71f1392
Andrew Oikonomidis <andyreneyload@gmail.com>
2018-01-07 20:39:04 +0200

    add mongoose-remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,20 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//      console.log(result);
+// });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findOneAndRemove({_id: '5a5268ae9b9989cd900a2b83'}).then((todo) => {
+    console.log(todo);
+});
+
+// Todo.findByIdAndRemove('5a5268ae9b9989cd900a2b83').then((todo) => {
+//     console.log(todo);
+// });
