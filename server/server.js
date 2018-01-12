11939cd9e37eb061fff5860e633c2ee0e648b3f4
Andrew Oikonomidis <andyreneyload@gmail.com>
2018-01-12 22:43:41 +0200

    add POST /users

--- server/server.js
+++ server/server.js
@@ -92,6 +92,15 @@ app.patch('/todos/:id', (req, res) => {
     }, (e) => res.status(400).send(e));
 });
 
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save().then((user) => {
+        res.send(user);
+    }, (e) => res.status(400).send(e));
+});
+
 app.listen(port, () => console.log(`Started on port ${port}`));
 
 module.exports = {app};
