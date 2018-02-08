00990f485845f1568acf71ac13daadb35339dce7
Andrew Oikonomidis <andyreneyload@gmail.com>
2018-02-08 02:22:54 +0200

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -30,7 +30,7 @@ describe('POST /todos', () => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
-                }).catch((e) => done(e));
+                }).catch(e => done(e));
             });
     });
 
@@ -47,7 +47,7 @@ describe('POST /todos', () => {
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(2);
                     done();
-                }).catch((e) => done(e));
+                }).catch(e => done(e));
             });
     });
 });
@@ -106,7 +106,7 @@ describe('DELETE /todos/:id', () => {
                 Todo.findById(hexId).then((todo) => {
                     expect(todo).toBeFalsy();
                     done();
-                }).catch((e) => done(e));
+                }).catch(e => done(e));
             });
     });
 
@@ -214,7 +214,7 @@ describe('POST /users', () => {
                     expect(user).toBeTruthy();
                     expect(user.password).not.toBe(password);
                     done();
-                });
+                }).catch(e => done(e));
             });
     });
 
@@ -239,4 +239,55 @@ describe('POST /users', () => {
             .expect(400)
             .end(done);
     });
-})
+});
+
+describe('POST /users/login', () => {
+    // it('should login user and return auth token', (done) => {
+    //     request(app)
+    //         .post('/users/login')
+    //         .send({
+    //             email: users[1].email,
+    //             password: users[1].password
+    //         })
+    //         .expect(200)
+    //         .expect((res) => {
+    //             expect(res.headers['x-auth']).toBeTruthy();
+    //         })
+    //         .end((err, res) => {
+    //             if (err) {
+    //                 return done(err);
+    //             }
+
+    //             User.findById(users[1]._id).then((user) => {
+    //                 expect(user.tokens[0]).toContain({
+    //                     access: 'auth',
+    //                     token: res.headers['x-auth']
+    //                 });
+    //                 done();
+    //             }).catch(e => done(e));
+    //         });
+    // });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toBeFalsy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+});
